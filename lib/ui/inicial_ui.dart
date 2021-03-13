4353a467c6da4f66ef92cbfc665e1e480b9355f2
Danyellias Manso <56615063+danyelliasmanso@users.noreply.github.com>
2021-03-13 15:43:19 -0300

    Alterando funcionalidade do botão "Voltar" do android

--- lib/ui/inicial_ui.dart
+++ lib/ui/inicial_ui.dart
@@ -39,221 +39,255 @@ class _PaginaInicialState extends State<PaginaInicial> {
     return alerta;
   }
 
+  Future<bool> _showExitDialog() {
+    return showDialog(
+        context: context,
+        builder: (item) => AlertDialog(
+              title: Text("Sair"),
+              content: Text("Você deseja realmente sair?"),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancelar")),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginPagina()));
+                    },
+                    child: Text(
+                      "Sair",
+                      style: TextStyle(color: Colors.red),
+                    ))
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Página Inicial"),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).primaryColor,
-        actions: <Widget>[
-          GestureDetector(
-            onTap: () {},
-            child: Icon(
-              Icons.notifications,
-            ),
-          ),
-          PopupMenuButton<String>(
-            itemBuilder: (BuildContext context) {
-              return {'Sobre', 'Sair'}.map((String choice) {
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();
-            },
-            onSelected: (value) {
-              if (value == 'Sair') {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return showAlertDialog(context);
-                  },
-                );
-              } else {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SobreTela()));
-              }
-            },
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Container(
-                width: 10,
-                height: 10,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Color(0xFFC3CFD9),
-                    width: 1,
-                  ),
-                  image: DecorationImage(
-                      fit: BoxFit.contain,
-                      image: AssetImage("assets/logo.png")),
-                ),
-              ),
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Página Inicial"),
+          centerTitle: true,
+          backgroundColor: Theme.of(context).primaryColor,
+          actions: <Widget>[
+            GestureDetector(
+              onTap: () {},
+              child: Icon(
+                Icons.notifications,
               ),
             ),
-            ListTile(
-              leading: Icon(AntIcons.inbox),
-              title: Text(
-                'Produtos',
-                style: TextStyle(color: Color(0xFF293845), fontSize: 20),
-              ),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ProdutosTela()));
+            PopupMenuButton<String>(
+              itemBuilder: (BuildContext context) {
+                return {'Sobre', 'Sair'}.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
               },
-            ),
-            ListTile(
-              leading: Icon(AntIcons.shop_outline),
-              title: Text(
-                'Estoque',
-                style: TextStyle(color: Color(0xFF293845), fontSize: 20),
-              ),
-            ),
-            ListTile(
-              leading: Icon(AntIcons.idcard_outline),
-              title: Text(
-                'Funcionários',
-                style: TextStyle(color: Color(0xFF293845), fontSize: 20),
-              ),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => FuncionarioTela()));
-              },
-            ),
-            ListTile(
-              leading: Icon(AntIcons.file_done),
-              title: Text(
-                'Marcas',
-                style: TextStyle(color: Color(0xFF293845), fontSize: 20),
-              ),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => MarcasTela()));
-              },
-            ),
-            ListTile(
-              leading: Icon(AntIcons.tags_outline),
-              title: Text(
-                'Categorias',
-                style: TextStyle(color: Color(0xFF293845), fontSize: 20),
-              ),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => CategoriasTela()));
+              onSelected: (value) {
+                if (value == 'Sair') {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return showAlertDialog(context);
+                    },
+                  );
+                } else {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SobreTela()));
+                }
               },
             ),
           ],
         ),
-      ),
-      body: ListView(children: <Widget>[
-        Container(
-          height: 60,
-          margin: EdgeInsets.fromLTRB(30, 185, 30, 0),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Color(0xFF02aed4),
-            borderRadius: BorderRadius.all(
-              Radius.circular(7),
-            ),
-          ),
-          child: SizedBox.expand(
-            child: TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Theme.of(context).primaryColor),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Text(
-                    "Clientes",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 25,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Color(0xFFC3CFD9),
+                      width: 1,
                     ),
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/logo.png")),
                   ),
-                ],
+                ),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                ),
               ),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ClienteTela()));
-              },
-            ),
+              ListTile(
+                leading: Icon(AntIcons.inbox),
+                title: Text(
+                  'Produtos',
+                  style: TextStyle(color: Color(0xFF293845), fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ProdutosTela()));
+                },
+              ),
+              ListTile(
+                leading: Icon(AntIcons.shop_outline),
+                title: Text(
+                  'Estoque',
+                  style: TextStyle(color: Color(0xFF293845), fontSize: 20),
+                ),
+              ),
+              ListTile(
+                leading: Icon(AntIcons.idcard_outline),
+                title: Text(
+                  'Funcionários',
+                  style: TextStyle(color: Color(0xFF293845), fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => FuncionarioTela()));
+                },
+              ),
+              ListTile(
+                leading: Icon(AntIcons.file_done),
+                title: Text(
+                  'Marcas',
+                  style: TextStyle(color: Color(0xFF293845), fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MarcasTela()));
+                },
+              ),
+              ListTile(
+                leading: Icon(AntIcons.tags_outline),
+                title: Text(
+                  'Categorias',
+                  style: TextStyle(color: Color(0xFF293845), fontSize: 20),
+                ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CategoriasTela()));
+                },
+              ),
+            ],
           ),
         ),
-        Container(
-          height: 60,
-          margin: EdgeInsets.fromLTRB(30, 20, 30, 0),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Color(0xFF02aed4),
-            borderRadius: BorderRadius.all(
-              Radius.circular(7),
+        body: ListView(children: <Widget>[
+          Container(
+            height: 60,
+            margin: EdgeInsets.fromLTRB(30, 185, 30, 0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color(0xFF02aed4),
+              borderRadius: BorderRadius.all(
+                Radius.circular(7),
+              ),
             ),
-          ),
-          child: SizedBox.expand(
-            child: TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Theme.of(context).primaryColor),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Vendas",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 25,
+            child: SizedBox.expand(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text(
+                      "Clientes",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
                     ),
-                  )
-                ],
+                  ],
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ClienteTela()));
+                },
               ),
-              onPressed: () => {},
             ),
           ),
-        ),
-        Container(
-          height: 60,
-          margin: EdgeInsets.fromLTRB(30, 20, 30, 0),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Color(0xFF02aed4),
-            borderRadius: BorderRadius.all(
-              Radius.circular(7),
-            ),
-          ),
-          child: SizedBox.expand(
-            child: TextButton(
-              style: TextButton.styleFrom(
-                  backgroundColor: Theme.of(context).primaryColor),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Orçamentos",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 25,
-                    ),
-                  )
-                ],
+          Container(
+            height: 60,
+            margin: EdgeInsets.fromLTRB(30, 20, 30, 0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color(0xFF02aed4),
+              borderRadius: BorderRadius.all(
+                Radius.circular(7),
               ),
-              onPressed: () => {},
+            ),
+            child: SizedBox.expand(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Vendas",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    )
+                  ],
+                ),
+                onPressed: () => {},
+              ),
             ),
           ),
-        ),
-      ]),
+          Container(
+            height: 60,
+            margin: EdgeInsets.fromLTRB(30, 20, 30, 0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color(0xFF02aed4),
+              borderRadius: BorderRadius.all(
+                Radius.circular(7),
+              ),
+            ),
+            child: SizedBox.expand(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Orçamentos",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    )
+                  ],
+                ),
+                onPressed: () => {},
+              ),
+            ),
+          ),
+        ]),
+      ),
+      onWillPop: () => _showExitDialog(),
     );
   }
 }
